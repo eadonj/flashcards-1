4ef99af8891f2bed17888e13612cec5177024176
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 12:44:38 -0700

    Updated the controller.

--- controller_and_view.rb
+++ controller_and_view.rb
@@ -1,21 +1,34 @@
 require 'flashcards.rb'
 
+# Controller
 deck = Deck.new
-puts "Let's have some fun with flashcards!"
 
-deck.cards.each do |card|
-  View.show(card)
+definition = show_definition
+respond_to_guess(definition)
+
+
+def show_definition
+  View.greeting
+  definition = deck.select_card
+  View.show(definition)
+  definition
+end
+
+def respond_to_guess(definition)
   guess = gets.chomp
-  check = deck.check_guess(card, guess)
+  check = deck.check_guess(definition, guess)
   View.response(check)
 end
 
-
 class View
 
-  def show(card)
+  def show(definition)
     puts "Definition: "
-    puts card.definition
+    puts definition
+  end
+
+  def greeting
+    puts "Let's have some fun with flashcards!"
   end
 
   def response(bool)
