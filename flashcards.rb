9f37c13788601e453d65db5f6792195455f6f3a0
Eadon <eadonj@gmail.com>
2013-05-24 12:24:05 -0700

    add class Card

--- flashcards.rb
+++ flashcards.rb
@@ -1,5 +1,8 @@
-class Deck
 
+
+class Deck
+  
+  attr_reader :cards
   def initialize
     @cards = # array: text import method 
     
@@ -10,21 +13,20 @@ class Deck
   end
 
   def check_guess(guess)
-
+    
   end
 
 end
 
 class Card
-
-  def initialize
-    @definition = #from text import
-    @answer = #from text import
+  
+  attr_reader :defintion, :answer
+  def initialize(args)
+    @definition = args.fetch(:defintion){raise "need card"}
+    @answer = args.fetch(:answer){raise "need answer"}
   end
-
 end
 
-Controller => 
-input = gets.chomp.downcase
 
-print = Deck.check_guess
+array = [["Designates code that must be run unconditionally at the beginning of the program before any other.", "BEGIN"],
+        ["Gives an unconditional termination to a code block, and is usually placed with an argument.", "break"]]
