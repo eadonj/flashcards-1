b369cbeb0be339b31f8159cbd11c194b1d595f93
jmiramant <josh@miramant.me>
2013-05-24 12:17:00 -0700

    message update

--- flashcards.rb
+++ flashcards.rb
@@ -1,4 +1,30 @@
-im here
-me too
+class Deck
 
-czech it out
+  def initialize
+    @cards = # array: text import method 
+    
+  end
+
+  def import
+    
+  end
+
+  def check_guess(guess)
+
+  end
+
+end
+
+class Card
+
+  def initialize
+    @definition = #from text import
+    @answer = #from text import
+  end
+
+end
+
+Controller => 
+input = gets.chomp.downcase
+
+print = Deck.check_guess
